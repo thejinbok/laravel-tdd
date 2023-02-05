b080282e43f290b7d1077fbba9492baa42ee916a
Jin Bok <jin@quatredeux.com>
2023-02-05 11:15:03 -0300

    refactor(Register): add request parameters validation in the controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterController extends Controller
 {
     public function __invoke()
     {
+        request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'unique:users', 'confirmed'],
+            'password' => ['required', Password::defaults()],
+        ]);
+
         $user = User::query()->create([
             'name' => request()->name,
             'email' => request()->email,
