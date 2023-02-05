8d1da9f998749c9a3440c9454a0838a6733d1988
Jin Bok <jin@quatredeux.com>
2023-02-05 07:02:51 -0300

    test(Feature): test if any user can invite someone to the platform via email

---
+++ tests/Feature/InvitationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Mail\Invitation;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
+use Tests\TestCase;
+
+class InvitationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_any_user_should_be_able_to_invite_someone_to_the_platform()
+    {
+        # Arrange
+        Mail::fake();
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        # Act
+        $this->post('invite', ['email' => 'new@email.com']);
+
+        # Assert
+        Mail::assertSent(Invitation::class, function ($mail) {
+            return $mail->hasTo('new@email.com');
+        });
+
+        $this->assertDatabaseHas('invites', ['email' => 'new@email.com']);
+    }
+}
