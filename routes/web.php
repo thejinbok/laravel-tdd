55f04511a875ba8bf288698cbef1a0afe6b8b70e
Jin Bok <jin@quatredeux.com>
2023-02-06 05:20:24 -0300

    feat(Todo): implement store and index feature

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\RegisterController;
 use App\Mail\Invitation;
 use App\Models\Invite;
+use App\Models\Todo;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
@@ -28,3 +29,18 @@ Route::post('invite', function () {
 
     Invite::create(['email' => request()->email]);
 })->name('invite');
+
+Route::get('todo', function () {
+    return view('todo', ['todos' => Todo::all()]);
+})->name('todo.index');
+
+Route::post('todo', function () {
+    Todo::query()
+        ->create([
+            'title' => request()->title,
+            'description' => request()->description,
+            'assigned_to_id' => request()->assigned_to,
+        ]);
+
+    return redirect()->route('todo.index');
+})->name('todo.store');
