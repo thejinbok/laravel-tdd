dcadca01f07921948c72e3874bc6119cd7363ec3
Jin Bok <jin@quatredeux.com>
2023-02-06 05:48:58 -0300

    test(Todo): test if a logged in user can attach a file to a todo item

--- tests/Feature/Todo/CreateTest.php
+++ tests/Feature/Todo/CreateTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Todo;
 
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class CreateTest extends TestCase
@@ -35,5 +37,25 @@ class CreateTest extends TestCase
             'description' => 'Todo Item Description',
             'assigned_to_id' => $assignedTo->id,
         ]);
+    }
+
+    public function test_a_logged_in_user_should_be_able_to_attach_a_file_to_a_todo_item()
+    {
+        # Arrange
+        Storage::fake('s3');
+
+        $user = User::factory()->createOne();
+        $this->actingAs($user);
+
+        # Act
+        $this->post(route('todo.store'), [
+            'title' => 'Todo Item',
+            'description' => 'Todo Item Description',
+            'assigned_to' => $user->id,
+            'file' => UploadedFile::fake()->image('fake-image.png'),
+        ]);
+
+        # Assert
+        Storage::disk('s3')->assertExists('todo/fake-image.png');
     }
 }
